fed7aeaf6419de6a74634c91ef793a008d61fc11
Rohit Tokala <rtokala@purdue.edu>
2023-02-09 16:48:09 -0500

    Add space bg

--- main.js
+++ main.js
@@ -61,6 +61,9 @@ function addStar() {
 
 Array(200).fill().forEach(addStar);
 
+const spaceTexture = new THREE.TextureLoader().load("space.jpg");
+scene.background = spaceTexture;
+
 function animate() {
   requestAnimationFrame(animate);
 
