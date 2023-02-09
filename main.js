59830d646c3225f5f61597b68e668001fc82c3a7
Rohit Tokala <rtokala@purdue.edu>
2023-02-09 16:31:41 -0500

    Rotate torus

--- main.js
+++ main.js
@@ -32,6 +32,11 @@ scene.add(torus);
 
 function animate() {
   requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
   renderer.render(scene, camera);
 }
 
